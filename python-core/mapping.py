39332aef71e2100d6a27aa9cd92d643d03e56b30
Areeb Ahmed <135235925+areebahmeddd@users.noreply.github.com>
2024-05-10 06:56:17 +0530

    🍔 [v0.6] Updated Folder Structure + Improved Logic

---
+++ python-core/mapping.py
@@ -0,0 +1,30 @@
+def group_name(nova_group):
+    group_names = {
+        1: 'Unprocessed or minimally processed foods',
+        2: 'Processed culinary ingredients',
+        3: 'Processed foods',
+        4: 'Ultra-processed food and drink products'
+    }
+    return group_names.get(nova_group, 'Unknown')
+
+def grade_color(nutriscore_grade):
+    grade_colors = {
+        'a': 'green',
+        'b': 'blue',
+        'c': 'yellow',
+        'd': 'orange',
+        'e': 'red'
+    }
+    return grade_colors.get(nutriscore_grade.lower(), 'gray')
+
+def score_color(nutriscore_score):
+    if nutriscore_score >= 90:
+        return 'green'
+    elif nutriscore_score >= 70:
+        return 'blue'
+    elif nutriscore_score >= 50:
+        return 'yellow'
+    elif nutriscore_score >= 30:
+        return 'orange'
+    else:
+        return 'red'
---
+++ python-core/utils.py
@@ -0,0 +1,47 @@
+def filter_ingredient(ingredient_data):
+    ingredient_info = [
+        {
+            'name': ingredient.get('text', '').title(),
+            'percentage': f"{abs(float(ingredient.get('percent_estimate', 0))):.2f} %"
+        }
+        for ingredient in ingredient_data
+        if ingredient.get('text') and ingredient.get('percent_estimate', 0) != 0
+    ]
+    return ingredient_info
+
+def analyse_nutrient(nutrient_data, nutrient_limit):
+    positive_nutrients = {}
+    negative_nutrients = {}
+
+    nutrient_items = {
+        nutrient: {
+            'name': nutrient.title(),
+            'quantity': f"{abs(float(nutrient_data.get(f'{nutrient}_100g', 0))):.2f} {value['unit']}"
+        }
+        for nutrient, value in nutrient_limit.items()
+        if nutrient_data.get(f'{nutrient}_100g', 0) != 0
+    }
+
+    for nutrient, value in nutrient_items.items():
+        lower_limit = nutrient_limit[nutrient]['lower_limit']
+        upper_limit = nutrient_limit[nutrient]['upper_limit']
+
+        if float(value['quantity'].split()[0]) < lower_limit or float(value['quantity'].split()[0]) > upper_limit:
+            negative_nutrients[nutrient] = value
+        else:
+            positive_nutrients[nutrient] = value
+
+    nutriment_info = {
+        'positive_nutrient': list(positive_nutrients.values()),
+        'negative_nutrient': list(negative_nutrients.values())
+    }
+    return nutriment_info
+
+def filter_image(image_data):
+    image_link = next(
+        iter(
+            list(image_data.values())[0].values()
+        ),
+        None
+    )
+    return image_link
