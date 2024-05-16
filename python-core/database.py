b03a7c87ca05bea1821d4e9b769cec94c9ded3ea
Areeb Ahmed <135235925+areebahmeddd@users.noreply.github.com>
2024-05-17 00:13:28 +0530

    Green-Elixir -> Scan-Easy Project name change to address broader markets such as cosmetics, pet food etc..

--- python-core/database.py
+++ python-core/database.py
@@ -12,21 +12,22 @@ def database_history(product_barcode, product_data):
         barcode_doc = api_history_ref.document(product_barcode)
 
         if barcode_doc.get().exists:
-            print(f'[Database] API history for "{product_barcode}" exists.')
+            print(f'[Database] Product history for "{product_barcode}" exists.')
             return
 
         barcode_doc.set(product_data)
-        print(f'[Database] API history for "{product_barcode}" stored.')
+        print(f'[Database] Product history for "{product_barcode}" stored.')
     except Exception as exc:
         print(f'Firestore storage error:\n {exc}')
 
-def search_database(search_keyword, search_keys):
+
+def database_search(product_keyword, search_keys):
     try:
         api_history_ref = db.collection('api_history')
 
         search_queries = [
-            api_history_ref.where(filter=FieldFilter(key, '>=', search_keyword))
-            .where(filter=FieldFilter(key, '<=', search_keyword + '\uf8ff'))
+            api_history_ref.where(filter=FieldFilter(key, '>=', product_keyword))
+            .where(filter=FieldFilter(key, '<=', product_keyword + '\uf8ff'))
             for key in search_keys
         ]
 
@@ -36,7 +37,7 @@ def search_database(search_keyword, search_keys):
             for doc in query.stream()
         ]
 
-        print(f'[Database] Found {len(found_documents)} document(s) for "{search_keyword}".')
+        print(f'[Database] Found {len(found_documents)} document(s) for "{product_keyword}".')
         return found_documents
     except Exception as exc:
         print(f'Database search error:\n {exc}')
