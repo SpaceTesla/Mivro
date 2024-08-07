eaba1e809daa7d8085bdee81275061184c46aaa6
Shivansh Karan <96942282+SpaceTesla@users.noreply.github.com>
2024-08-07 14:38:08 +0530

    FIXES #97 🍓 [v2.3.1] Improved UI in Popup Fixed these things - Fix the icon - Add div for each tab - Make the divs update dynamically

--- browser-extension/popup/popup.js
+++ browser-extension/popup/popup.js
@@ -1,9 +1,16 @@
 const list = document.querySelectorAll(".nav-list li");
-const nav = document.querySelector(".navigation");
+const containers = document.querySelectorAll(".container");
 
-list.forEach((item) => {
+list.forEach((item, index) => {
   item.addEventListener("click", function (e) {
+    // Remove active class from all list items
     list.forEach((li) => li.classList.remove("active"));
+    // Add active class to the clicked list item
     e.currentTarget.classList.add("active");
+
+    // Hide all containers
+    containers.forEach((container) => container.classList.add("hidden"));
+    // Show the corresponding container
+    containers[index].classList.remove("hidden");
   });
 });
