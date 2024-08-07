ad35ba1514d11c1848494023f3f4d0b6508a9873
Shivansh Karan <96942282+SpaceTesla@users.noreply.github.com>
2024-08-07 19:41:24 +0530

    🍟 [v2.3.6] Added Hover Effect to Popup

--- browser-extension/popup/popup.js
+++ browser-extension/popup/popup.js
@@ -8,6 +8,20 @@ list.forEach((item, index) => {
     // Add active class to the clicked list item
     e.currentTarget.classList.add("active");
 
+    // Add grey class to all li > a > img elements
+    list.forEach((li) => {
+      const img = li.querySelector("a > img");
+      if (img) {
+        img.classList.add("grey");
+      }
+    });
+
+    // Remove grey class from the img element inside the clicked li > a
+    const clickedImg = e.currentTarget.querySelector("a > img");
+    if (clickedImg) {
+      clickedImg.classList.remove("grey");
+    }
+
     // Hide all containers
     containers.forEach((container) => container.classList.add("hidden"));
     // Show the corresponding container
